933ede7c1474df1b4f57662f507a11f812195970
L0rdSaiyan <89421533+L0rdSaiyan@users.noreply.github.com>
2022-02-06 18:55:58 -0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,5 @@
+dia = input('Informe o dia de seu nascimento: ')
+mes = input('Informe o mês de seu nascimento: ') 
+ano = input('Informe o ano de seu nascimento: ')
+
+print("\nData formatada: \n\n"+dia+"/"+mes+"/"+ano) 
